16ccd8c42ee6d51668f12815d876189c9712c150
James Pederson <james@jpederson.com>
2014-09-04 22:15:24 -0500

    Adding title back into page template, styling grey bar at all resolutions, and fixing search box alignment.

--- page.php
+++ page.php
@@ -9,8 +9,14 @@ get_header();
 	<main class="content-narrow">
 
 		<?php 
-		while ( have_posts() ) : the_post();
-			the_content();
+		while ( have_posts() ) : the_post(); ?>
+		
+		<h1><?php the_title() ?></h1>
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
+
+			<?php
 		endwhile; 
 		?>
 
