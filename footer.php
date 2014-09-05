8e6312363b70753b182b87ab7ecd1bb659daf991
James Pederson <james@jpederson.com>
2014-09-04 22:30:51 -0500

    Fixing footer text to look nice at all resolutions and adapt nicely.

--- footer.php
+++ footer.php
@@ -26,13 +26,17 @@
 	</footer><!-- #colophon -->
 	
 	<div class="colophon">
-
-		<strong>Physical Address:</strong><br>
-			<a href="https://www.google.com/maps/place/300+Seward+St,+Ripon,+WI+54971/@43.8432217,-88.8409595,17z/data=!3m1!4b1!4m2!3m1!1s0x8801543f4dcd4019:0xbdb7efb240144666">300 W. Seward St. Ripon, WI</a><br>
-		<strong>Mailing Address:</strong><br>
-			PO Box 248 Ripon, WI 54971-0248<br>
-		<strong>Email:</strong> <a href="adminfo@ripon.edu">adminfo@ripon.edu</a><br>
-		<strong>Phone:</strong> 800-947-4766<br><br>
+		
+		<div class="addresses">
+			<strong>Physical Address:</strong><br>
+				<a href="https://www.google.com/maps/place/300+Seward+St,+Ripon,+WI+54971/@43.8432217,-88.8409595,17z/data=!3m1!4b1!4m2!3m1!1s0x8801543f4dcd4019:0xbdb7efb240144666">300 W. Seward St. Ripon, WI</a><br>
+			<strong>Mailing Address:</strong><br>
+				PO Box 248 Ripon, WI 54971-0248<br>
+		</div>
+		<div class="electronic">
+			<strong>Email:</strong> <a href="adminfo@ripon.edu">adminfo@ripon.edu</a><br>
+			<strong>Phone:</strong> 800-947-4766
+		</div>
 		
 		<a href="#" class="campus-map">Campus Map</a>
 
