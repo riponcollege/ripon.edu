74df67c295a48681d31f191bfdf34fb47373cc09
James Pederson <james@jpederson.com>
2014-10-23 14:29:19 -0700

    Check that a menu has been selected before displaying it on the front-end.

--- library/menus.php
+++ library/menus.php
@@ -45,12 +45,18 @@ if ( function_exists('register_sidebar') ) {
 
 function left_menu_display( $position = 'primary' ) {
 
+    // grab the menu the user selected in the menus metabox.
     $menu_name = get_post_meta( get_the_ID(), CMB_PREFIX . "menu_" . $position, 1 );
 
-	wp_nav_menu( array( 
-		'menu' => $menu_name, 
-		'menu_class' => 'nav-menu' )
-	);
+    // verify that the menu exists by checking the menu name to see if it's empty
+    if ( !empty( $menu_name ) ) {
+
+        // display the menu
+        wp_nav_menu( array( 
+            'menu' => $menu_name, 
+            'menu_class' => 'nav-menu' )
+        );
+    }
 
 }
 
