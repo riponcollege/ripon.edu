b86e05216ede39b2d4c23d29f49e39bdb8ff0e5b
James Pederson <james@jpederson.com>
2015-02-05 16:17:38 -0600

    Adds correct icon to search submit buttons, after pulling in all the icon classes.

--- searchform-faculty.php
+++ searchform-faculty.php
@@ -2,6 +2,6 @@
 	<form role="search" method="get" id="searchform" class="searchform faculty-search" action="/faculty" _lpchecked="1">
 		<input type="text" value="<?php print ( isset( $_REQUEST['s'] ) ? $_REQUEST['s'] : "" ) ?>" name="s" id="s" placeholder="Search Name, Academic Department Or Area of Interest">
 		<input type="hidden" name="post_type" value="faculty" />
-		<input type="submit" id="searchsubmit" value="Search">
+		<button type="submit" id="searchsubmit"><i class="fa fa-lg fa-search"></i></button>
 	</form>
 </div>
--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form role="search" method="get" id="searchform" class="searchform" action="/" _lpchecked="1">
 	<input type="hidden" name="post_type" value="area" />
 	<input type="text" value="" name="s" id="s" placeholder="Search Ripon">
-	<input type="submit" id="searchsubmit" value="Search">
+	<button type="submit" id="searchsubmit"><i class="fa fa-lg fa-search"></i></button>
 </form>
