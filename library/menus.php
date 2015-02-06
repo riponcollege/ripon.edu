c7e68d8135ee75ffc3a687cf60f75a8732937f92
James Pederson <james@jpederson.com>
2015-02-06 02:37:35 -0600

    Adjust widgets and set styles for icons that show up above each of the types of widgets.

--- library/menus.php
+++ library/menus.php
@@ -28,7 +28,7 @@ if ( function_exists('register_sidebar') ) {
 	register_sidebar(array(
 		'name'=> 'General Sidebar',
 		'id' => 'sidebar-generic',
-		'before_widget' => '<div class="widget">',
+		'before_widget' => '<div class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h4>',
 		'after_title' => '</h4>',
@@ -36,7 +36,7 @@ if ( function_exists('register_sidebar') ) {
 	register_sidebar(array(
 		'name'=> 'Home Spotlight',
 		'id' => 'spotlight',
-		'before_widget' => '<div class="widget">',
+		'before_widget' => '<div class="widget %2$s">',
 		'after_widget' => '</div>',
 		'before_title' => '<h4>',
 		'after_title' => '</h4>',
