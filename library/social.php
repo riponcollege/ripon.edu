52f0795dad6a188a2e7e50455ebc6eca37786152
James Pederson <james@jpederson.com>
2017-04-03 11:36:36 -0500

    Un-include the twitteroauth api script, and set a height on the twitter widget so that it is a scrollable div.

--- library/social.php
+++ library/social.php
@@ -1,7 +1,7 @@
 <?php
 
 
-require_once( 'twitter/twitteroauth/twitteroauth.php' );
+// require_once( 'twitter/twitteroauth/twitteroauth.php' );
 
 
 
@@ -23,7 +23,7 @@ function twitter_posts() {
 	}
 	*/
 	?>
-	<a class="twitter-timeline" href="https://twitter.com/riponcollege" data-chrome="nofooter noborders">Tweets by riponcollege</a>
+	<a class="twitter-timeline" href="https://twitter.com/riponcollege" data-chrome="nofooter noborders" height="400">Tweets by riponcollege</a>
 	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 	<?php
 }
