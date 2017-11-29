542522d52ab61f6d5e8840a40184423b90c4aca5
James Pederson <james@jpederson.com>
2017-11-29 03:42:30 -0600

    Bump stylesheet to force cache break.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=111" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=112" rel="stylesheet" type="text/css">
 
 </head>
 <body <?php body_class(); ?>>
