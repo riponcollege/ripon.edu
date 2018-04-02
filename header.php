e952b0d0ad58d7c3d27e62f74ba62ceceaa4da2e
James Pederson <james@jpederson.com>
2018-04-02 05:12:04 -0500

    Bump CSS to force cache break.

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
-<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=117" rel="stylesheet" type="text/css">
+<link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=118" rel="stylesheet" type="text/css">
 
 <!-- Facebook Pixel Code -->
 <script>
