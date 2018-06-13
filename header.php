63d0877a6c0c8b7a6c58055aea73c39debb3985a
James Pederson <james@jpederson.com>
2018-06-13 15:52:53 -0500

    Add Google Tag Manager.

--- header.php
+++ header.php
@@ -50,9 +50,20 @@ gtag('config', 'UA-37190446-2');
 </script>
 <!-- End Analytics Code -->
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5Q9X26');</script>
+<!-- End Google Tag Manager -->
+
 </head>
 <body <?php body_class(); ?>>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Q9X26"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header>
 
 	<div class="wrap">
