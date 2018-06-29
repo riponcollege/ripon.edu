8dee314eeddea4de38fac1e18bd0c00b1428f37f
James Pederson <james@jpederson.com>
2018-06-29 06:35:34 -0500

    Remove PO Box from footer.

--- footer.php
+++ footer.php
@@ -32,7 +32,6 @@
 		
 		<div class="addresses">
 			<strong>Physical Address:</strong> <span class="fold"><a href="https://www.google.com/maps/place/300+Seward+St,+Ripon,+WI+54971/@43.8432217,-88.8409595,17z/data=!3m1!4b1!4m2!3m1!1s0x8801543f4dcd4019:0xbdb7efb240144666">300 W. Seward St. Ripon, WI</a></span><br>
-			<strong>Mailing Address:</strong> <span class="fold">PO Box 248 Ripon, WI 54971-0248</span>
 		</div>
 		<div class="electronic">
 			<strong>Email:</strong> <span class="fold"><a href="mailto:adminfo@ripon.edu">adminfo@ripon.edu</a></span><br>
