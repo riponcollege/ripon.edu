e7110353db85df3f7c751178a2775b738c9fdc98
James Pederson <james@jpederson.com>
2018-11-01 15:14:40 -0500

    Add trustees template and styles, and add zipcode to footer address.

--- footer.php
+++ footer.php
@@ -32,7 +32,7 @@
 		
 		<div class="addresses">
 			<strong>Physical Address:</strong><br>
-			<span class="fold"><a href="https://www.google.com/maps/place/300+Seward+St,+Ripon,+WI+54971/@43.8432217,-88.8409595,17z/data=!3m1!4b1!4m2!3m1!1s0x8801543f4dcd4019:0xbdb7efb240144666">300 W. Seward St. Ripon, WI</a></span><br>
+			<span class="fold"><a href="https://www.google.com/maps/place/300+Seward+St,+Ripon,+WI+54971/@43.8432217,-88.8409595,17z/data=!3m1!4b1!4m2!3m1!1s0x8801543f4dcd4019:0xbdb7efb240144666">300 W. Seward St. Ripon, WI 54971</a></span><br>
 			<a href="/privacy-policy">Privacy Policy</a>
 		</div>
 		<div class="electronic">
