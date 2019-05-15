0e65b7587d7f886ac2aba563ca3a8ab707dd0e2f
James Pederson <james@jpederson.com>
2019-05-15 18:17:40 -0500

    Add 'color' parameter to the button shortcode, and create styles for all the existing brand color variables.

--- library/buttons.php
+++ library/buttons.php
@@ -3,11 +3,12 @@
 
 function button_shortcode( $atts, $content = null ) {
 	$a = shortcode_atts( array(
-		'url' => ''
+		'url' => '',
+		'color' => 'green'
 	), $atts );
 
 	if ( !empty( $a['url'] ) && !empty( $content ) ) {
-		return '<a href="' . $a['url'] . '" class="btn">' . $content . '</a>';
+		return '<a href="' . $a['url'] . '" class="btn ' . $a['color'] . '">' . $content . '</a>';
 	}
 }
 add_shortcode( 'button', 'button_shortcode' );
