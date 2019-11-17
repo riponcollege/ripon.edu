fd5ea8c54aa4022840d3a135bd117fc37d00b88e
James Pederson <james@jpederson.com>
2019-11-17 16:46:25 -0600

    Add logic to only output tiles that have content.

--- library/phototiles.php
+++ library/phototiles.php
@@ -6,17 +6,19 @@ function the_phototiles() {
 	if ( !empty( $tiles ) ) {
 		?>
 		<div class="phototiles">
-		<?php	
+		<?php
 		foreach ( $tiles as $tile ) {
-			if ( !empty( $tile['link'] ) ) { ?><a href="<?php print $tile['link'] ?>"><?php } 
-			?>
+			if ( !empty( $tile['title'] ) && !empty( $tile['background'] ) && !empty( $tile['content'] ) ) {
+				if ( !empty( $tile['link'] ) ) { ?><a href="<?php print $tile['link'] ?>"><?php } 
+				?>
 			<div class="phototile" style="background-image: url(<?php print $tile['background'] ?>);">
 				<div class="phototile-title"><?php print $tile['title'] ?></div>
 				<div class="phototile-subtitle"><?php print $tile['subtitle'] ?></div>
 				<div class="phototile-content <?php print $tile['color'] ?>"><?php print $tile['content'] ?></div>
 			</div>
-			<?php
-			if ( !empty( $tile['link'] ) ) { ?></a><?php } 
+				<?php
+				if ( !empty( $tile['link'] ) ) { ?></a><?php } 
+			}
 		}
 	}
 }
